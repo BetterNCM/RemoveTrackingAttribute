4ab655c7b01d30e1d8de6f22f31ca7972a971c31
MicroBlock <66859419+MicroCBer@users.noreply.github.com>
2023-06-23 20:45:09 +0800

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ plugin.onLoad(() => {
 	const _channalCall = channel.call;
 	channel.call = (name, ...args) => {
 		if (name === 'winhelper.setClipBoardData') {
-			args[1][0] = args[1][0].replace(/&userid=\d+/, '');
+			args[1][0] = args[1][0].replace(/userid=\d+/, '');
 		}
 		_channalCall(name, ...args);
 	};
